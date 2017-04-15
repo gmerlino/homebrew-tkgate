62fb551b3d86f237f36f90d974001084e937cd77
Giovanni Merlino <gmerlino@unime.it>
2017-04-15 22:58:17 +0200

    No more segfaulting: it needs X11-based TK

--- tkgate.rb
+++ tkgate.rb
@@ -6,19 +6,15 @@ class Tkgate < Formula
 
   depends_on "cairo" => "with-x11"
   depends_on "pango" => "with-x11"
-  depends_on "tcl-tk"
+  depends_on "gmerlino/tcl-tk/tcl-tk" => "with-x11"
 
   patch :DATA
 
   def install
-#    ENV.prepend_path "PKG_CONFIG_LIBDIR", "#{Formula["tcl-tk"].opt_lib}/pkgconfig"
-#    ENV.append_path "PKG_CONFIG_PATH", "#{Formula["tcl-tk"].opt_lib}/pkgconfig"
-#    ENV.append 'TCL_INCLUDE_SPEC', "-I#{Formula["tcl-tk"].opt_include}"
-#    ENV.append 'TK_INCLUDE_SPEC', "-I#{Formula["tcl-tk"].opt_include}"
-#    ENV.append 'TKGATE_IPATH', "-I#{Formula["tcl-tk"].opt_include}"
-#    ENV.append 'TCL_IPATH', "-I#{Formula["tcl-tk"].opt_include}"
-#    ENV.append 'TK_IPATH', "-I#{Formula["tcl-tk"].opt_include}"
-    ENV.append 'CPPFLAGS', "-DUSE_INTERP_RESULT"
+    ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["tcl-tk"].opt_lib}/pkgconfig"
+    ENV.append 'TCL_IPATH', "-I#{Formula["tcl-tk"].opt_include}"
+    ENV.append 'TCL_LPATH', "-L#{Formula["tcl-tk"].opt_lib}"
+    ENV.append 'CPPFLAGS', "-g"
     system "./configure",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
@@ -35,7 +31,13 @@ __END__
 diff --git a/configure b/configure
 --- a/configure
 +++ b/configure
-@@ -2916,7 +2916,7 @@
+@@ -2911,12 +2911,12 @@
+ #
+ # Directories to search for include files
+ #
+-TKGATE_INCDIRS="/usr/X11R6/include /usr/X11/include /pkgs/include /usr/local/include /usr/openwin/include /usr/X/include /usr/include /sw/include /opt/local/include"
++TKGATE_INCDIRS="/usr/local/opt/tcl-tk/include /usr/X11R6/include /usr/X11/include /pkgs/include /usr/local/include /usr/openwin/include /usr/X/include /usr/include /sw/include /opt/local/include"
+
  #
  # Directories to search for library files
  #
@@ -53,15 +55,18 @@ diff --git a/configure b/configure
        iconv_lib_dir=$p
      fi
    done
-@@ -6818,10 +6818,10 @@ $as_echo_n "checking tcl/tk version... " >&6; }
+@@ -6817,11 +6817,11 @@
+	  TCLTK_VERSION=$TCL_VERSION
 
    TCL_LIB=$TCL_LIB_SPEC
-   TCL_LPATH=""
+-  TCL_LPATH=""
 -  TCL_IPATH=$TCL_INCLUDE_SPEC
++  #TCL_LPATH=""
 +  #TCL_IPATH=$TCL_INCLUDE_SPEC
    TK_LIB=$TK_LIB_SPEC
-   TK_LPATH=""
+-  TK_LPATH=""
 -  TK_IPATH=$TK_INCLUDE_SPEC
++  #TK_LPATH=""
 +  #TK_IPATH=$TK_INCLUDE_SPEC
 
    if test "X$TCL_IPATH" = "X"; then
